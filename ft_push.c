240c4845a2a5324468d1b58072e31a31986b2f0d
thiasix <prg202@gmail.com>
2026-06-09 13:38:44 +0200

    push operator pseudocode

---
+++ ft_push.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_push.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: patperez <patperez@student.42urduliz.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/09 13:04:38 by patperez          #+#    #+#             */
+/*   Updated: 2026/06/09 13:38:12 by patperez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include
+
+void	ft_pa(t_list **stack_a, t_list **stack_b)
+{
+	if (!stack_a || !stack_b)
+		return ;
+	if (stack_b == 0)
+		return ;
+	stack_a -> next = *
+}
+
+int	main(void)
+{
+	t_list	*stack_a;
+	t_list	*stack_b;
+
+	return (0);
+}
---
+++ ft_pushswap_lib.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pushswap_lib.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: patperez <patperez@student.42urduliz.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/09 13:24:41 by patperez          #+#    #+#             */
+/*   Updated: 2026/06/09 13:25:40 by patperez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSHSWAPLIB_H
+# define PUSHSWAPLIB_H
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct	s_list
+{
+	void	*content;
+	struct	s_list *next;
+} t_list;
+
+#endif
+
