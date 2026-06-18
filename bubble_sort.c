caf9c011af02fec22c71fbe1a57f9ba4f4837f8a
thiasix <prg202@gmail.com>
2026-06-18 15:38:05 +0200

    next step

---
+++ bubble_sort.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bubble_sort.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: patperez <patperez@student.42urduliz.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/18 12:43:35 by patperez          #+#    #+#             */
+/*   Updated: 2026/06/18 13:50:57 by patperez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pushswaplib.h"
+
+static void	swapper()
+{
+	
+}
+
+void	bubble_sort(int input[])
+{
+	while (input[])
+		{
+			while (input[i] > input [i + 1])
+				{
+					swap
+				}
+		}
+}
