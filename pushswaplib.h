10a29655ea149d820bae08349446c646d4c6be28
Unai Garcia Morchon <ugarcia-@c3r3s3.42urduliz.com>
2026-06-25 15:00:53 +0200

    subida con cambios en test

--- pushswaplib.h
+++ pushswaplib.h
@@ -6,7 +6,7 @@
 /*   By: ugarcia- <ugarcia-@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/09 13:24:41 by patperez          #+#    #+#             */
-/*   Updated: 2026/06/24 08:03:24 by ugarcia-         ###   ########.fr       */
+/*   Updated: 2026/06/25 13:51:52 by ugarcia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ typedef struct node
 {
 	int				content;
 	struct node		*next;
+	//struct node		*prev;
 }					t_node_list;
 
 /* OPERATIONS */
--- test.c
+++ test.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   INVALID_bubble_sort.c                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ugarcia- <ugarcia-@student.42urduliz.com>  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/18 12:43:35 by patperez          #+#    #+#             */
-/*   Updated: 2026/06/25 12:36:59 by ugarcia-         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "pushswaplib.h"
 
 double	sqroot(int x)
@@ -38,6 +26,7 @@ double	sqroot(int x)
 	return (y);
 }
 
+
 int	ft_max_number(t_node_list **lst_a)
 {
 	int	temp;
@@ -47,13 +36,15 @@ int	ft_max_number(t_node_list **lst_a)
 	temp = (*lst_a) -> content;
 	while (temp_list -> next != NULL)
 	{
-		if (temp > temp_list -> next -> content)
+		if (temp < temp_list -> content)
 		{
 			temp = temp_list -> content;
 		}
 		temp_list = temp_list -> next;
 	}
+	printf("TEMPmax>>>>> %i\n", temp);
 	return (temp);
+
 }
 
 int	ft_min_number(t_node_list **lst_a)
@@ -71,10 +62,12 @@ int	ft_min_number(t_node_list **lst_a)
 		}
 		temp_list = temp_list -> next;
 	}
-	printf("TEMP>>>>> %i\n", temp);
+	printf("TEMPmin>>>>> %i\n", temp);
 	return (temp);
-	
+
 }
+
+
 
 void	ft_bucket(t_node_list **lst_a)
 {
@@ -84,14 +77,13 @@ void	ft_bucket(t_node_list **lst_a)
 	int bucket_number; // Para guardar el tamaño del bloque (raíz cuadrada)
 	int number_limit; // Para guardar tu "número límite" actual
 	int	end_of_bucket;
-	//int	number_max;
-
+	int	number_max;
+	
 	if (lst_a == NULL || *lst_a == NULL || (*lst_a) -> next == NULL)
 		return ;
 	lst_b =  malloc(sizeof(t_node_list));
 	number_min = ft_min_number(lst_a);
-	printf("number min :%d\n", number_min);
-	//number_max = ft_max_number(lst_a);
+	number_max = ft_max_number(lst_a);
 	total = ft_lstsize (*lst_a);
 	bucket_number = sqroot(total);
 	number_limit = (bucket_number + number_min);
@@ -100,54 +92,86 @@ void	ft_bucket(t_node_list **lst_a)
 	printf("nmber limit %i\n", number_limit);
 	printf("nmber %i\n", number_min);
 	printf("bucket %i\n", bucket_number);
-	while ((*lst_a) -> next)
+	printf("List_B  %d\n", (*lst_b)->content);
+	int	temp2 = 0;
+	while ((*lst_b) -> next != NULL)
 	{
-		while ((*lst_a) -> content > number_limit)
+		temp2 ++;
+		printf("temporal2  %d\n", temp2);
+		*lst_b = (*lst_b) -> next;
+	}
+	while ((*lst_a)-> next)
+	{
+		while ((*lst_a) -> content < number_limit)
 		{
 			ft_pb(lst_b, lst_a);
-			printf("push -> b %i\n", (*lst_b) -> content);
-			if ((*lst_b) -> next != NULL)
-			{
-				while ((*lst_b) -> content < (*lst_b) -> next -> content)
-				{
-					if ((*lst_b) -> content < (*lst_b) -> next -> content)
-						ft_sb(lst_b);
-					//ft_rb(lst_b);
-				}
-			}
+			printf("push %i\n", (*lst_b) -> content);
 		}
+		printf("google %i\n", number_limit);
 		if ((*lst_a) -> content == end_of_bucket)
 		{
 			number_limit = number_limit + bucket_number;
-			printf("NUMBER limit %i\n", number_limit);
+			printf("nmber limit %i\n", number_limit);
 		}
 		if ((*lst_a) -> next != NULL)
-		{
-			ft_pb(lst_b, lst_a);
-		}
-		else
 			ft_ra(lst_a);
+		printf("papta %i\n", number_limit);
 	}
-	/* if((*lst_a) == number_max)
+	if((*lst_a) -> content == number_max)
 	{
-		while ((*lst_b))
+		printf("last node %i\n", (*lst_a) -> content);
+	}
+	/* while ((*lst_b))
 		{
 			ft_pa();
-		}
+		} */
 
-	} */
-
-	printf("First B node: %d\n", (*lst_b) -> content);
+/* 	printf("First B node: %d\n", (*lst_b) -> content);
 	printf("Second B node: %d\n", (*lst_b) -> next -> content);
 	printf("Third B node: %d\n", (*lst_b) -> next -> next -> content);
 	printf("Fourth B node: %d\n", (*lst_b) -> next -> next -> next -> content);
 	printf("Fifth B node: %d\n", (*lst_b) -> next -> next -> next -> next -> content);
 	printf("Sixth B node: %d\n", (*lst_b) -> next -> next -> next -> next -> next -> content);
-	printf("siete B node: %d\n", (*lst_b) -> next -> next -> next -> next -> next -> next -> content);
-	printf("ocho B node: %d\n", (*lst_b) -> next -> next -> next -> next -> next -> next-> next -> content);
-	printf("nueve B node: %d\n\n", (*lst_b) -> next -> next -> next -> next -> next -> next-> next -> next -> content);
-	
-}*/
+	printf("siete B node: %d\n", (*lst_b) -> next -> next -> next -> next -> next -> next -> content); */
+	//printf("ocho B node: %d\n\n", (*lst_b) -> next -> next -> next -> next -> next -> next ->  next ->content);
+}
+
+//void lstnode(lista_ref, cant_nodos) para hacer la lista bien problema que creo que es la lista vacia del b crear los nodos vacios
+// comprobar que la lista B esta a null
+// hacer el prev ayuda mucho.
+
+
+
+/* void	lst_create_new_node(t_node_list lst_ref, int number_nodos)
+{
+	//t_node_list	*lst_a;
+	t_node_list	*lst_b;
+	int			i;
+
+	//lst_a = NULL;
+	lst_b = NULL;
+	i = 0;
+	while (i < number_nodos)
+	{
+		ft_lstadd_back(&lst_b, ft_lstnew(lst_ref.content));
+		i++;
+	}
+} */
+void	lst_create_new_node(t_node_list lst_ref, int number_nodos)
+{
+	//t_node_list	*lst_a;
+	t_node_list	*lst_b;
+	int			i;
+
+	//lst_a = NULL;
+	lst_b = NULL;
+	i = 0;
+	while (i < number_nodos)
+	{
+		ft_lstadd_back(&lst_b, ft_lstnew(lst_ref.content));
+		i++;
+	}
+}
 
 int	main(void)
 {
@@ -160,19 +184,19 @@ int	main(void)
 	ft_lstadd_back(&lst_a, ft_lstnew(1));
 	ft_lstadd_back(&lst_a, ft_lstnew(13));
 	ft_lstadd_back(&lst_a, ft_lstnew(16));
-	ft_lstadd_back(&lst_a, ft_lstnew(18));
 	ft_lstadd_back(&lst_a, ft_lstnew(2));
-	ft_lstadd_back(&lst_a, ft_lstnew(9));
+	ft_lstadd_back(&lst_a, ft_lstnew(18));
+	/*ft_lstadd_back(&lst_a, ft_lstnew(9)); */
 
-	printf("First A node: %d\n", lst_a -> content);
-	printf("Second A node: %d\n", lst_a -> next -> content);    
+	/* printf("First A node: %d\n", lst_a -> content);
+	printf("Second A node: %d\n", lst_a -> next -> content);
 	printf("Third A node: %d\n", lst_a -> next -> next -> content);
 	printf("Fourth A node: %d\n", lst_a -> next -> next -> next -> content);
 	printf("Fifth A node: %d\n", lst_a -> next -> next -> next -> next -> content);
 	printf("Sixth A node: %d\n", lst_a -> next -> next -> next -> next -> next -> content);
-	printf("siete A node: %d\n", lst_a -> next -> next -> next -> next -> next -> next -> content);
-	printf("ocho A node: %d\n", lst_a -> next -> next -> next -> next -> next -> next-> next -> content);
-	printf("nueve A node: %d\n\n", lst_a -> next -> next -> next -> next -> next -> next-> next -> next -> content);
+	printf("siete A node: %d\n", lst_a -> next -> next -> next -> next -> next -> next -> content); */
+	//printf("ocho A node: %d\n", lst_a -> next -> next -> next -> next -> next -> next-> next -> content);
+	/*printf("nueve A node: %d\n\n", lst_a -> next -> next -> next -> next -> next -> next-> next -> next -> content); */
 	
 
 	ft_bucket(&lst_a);
@@ -185,36 +209,14 @@ int	main(void)
 	//printf("Sixth A node: %d\n", lst_a -> next -> next -> next -> next -> next -> content);
 */
 	ft_lstclear(&lst_a);
-	return (0);
-}
-
-/*
-while (lst_a)
+	
+	int	temp = 0;
+	while (lst_a -> next != NULL)
 	{
-		if ((*lst_a) -> content < (*lst_a) -> next -> content ||
-			(*lst_a) -> next == NULL)
-		{
-			ft_pb(lst_b, lst_a);
-			if ((*lst_b) -> content < (*lst_b) -> next -> content)
-				ft_sb(lst_b);
-			else
-				while ((*lst_b) -> content > (*lst_b) -> next -> content)
-					ft_rb(lst_b);
-		}
-		else
-			ft_ra(lst_a);
+		temp ++;
+		lst_a = lst_a -> next;
 	}
-	while (lst_b)
-		ft_pa(lst_a, lst_b);
-	ft_lstclear(lst_b);
-*/
+	printf("temporal  %d\n", temp);
+	return (0);
 
-/*
-		while ((bucket + number) < (*lst_a) -> content)
-		{
-			ft_pb;
-		}
-		(bucket + number) + bucket;
-	}
-*/
-
+}
