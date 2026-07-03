6225542a34475d4e2c1410943974c4e6216f5de2
Unai Garcia Morchon <ugarcia-@c3r3s3.42urduliz.com>
2026-07-03 12:46:20 +0200

    en pushswap en main concatenado en un string? pero todo junto

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ugarcia- <ugarcia-@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/18 12:21:30 by patperez          #+#    #+#             */
-/*   Updated: 2026/07/03 12:03:00 by ugarcia-         ###   ########.fr       */
+/*   Updated: 2026/07/03 12:45:15 by ugarcia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,28 @@
 /*
 convertirlo a string separados por espacios, concatenar.
 trocear hacerle el split?. trocerar
-validar que sea números osea convertirlos a números isdigit? validar
+validar que sea números, convertirlos a números isdigit? validar
 sin duplicados, sin negativos, no se salgan de los limites, atoi? convertir
 */
 
 int	main(int argc, char **argv)
 {
-	char	*dest;
+	char	dest[50] = "";
+	int		i;
+	char	string;
+
+	i = 1;
+	while (i < argc)
+	{
+		string = ft_strlcat(dest, argv[i], 50);
+		i++;
+	}
 
 	if (argc < 2)
 		return (1);
-	dest = 0;
 	printf("Número total de argumentos: %d\n", argc);
-	//printf("El primer número es: %s\n", argv[1]);
-	printf("string destino: %ld\n", ft_strlcat(dest, *argv, argc));
+	printf("El primer número es: %s\n", argv[1]);
+	printf("total string size: %d\n", string);
+	printf("string destino: %s\n", dest);
 	return (0);
 }
