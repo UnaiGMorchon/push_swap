5008051ea85c1c7e75762862bdc0bdf984b0e2b0
thiasix <prg202@gmail.com>
2026-07-06 13:10:27 +0200

    error checks for console input done for now

--- ft_input_conversion.c
+++ ft_input_conversion.c
@@ -6,21 +6,59 @@
 /*   By: patperez <patperez@student.42urduliz.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/19 09:58:07 by patperez          #+#    #+#             */
-/*   Updated: 2026/06/19 10:03:35 by patperez         ###   ########.fr       */
+/*   Updated: 2026/07/06 13:08:43 by patperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pushswaplib.h"
 
-void	input_conversion(input array)
+int	ft_is_validint(char *input)
 {
 	int	i;
-	t_node_list	lst;
 
 	i = 0;
-	while (input array)
+	if (input[i] == '-')
+		i++;
+	while (input[i] != '\0')
 	{
-		ft_lstadd_back(lst, input array[i]);
+		if (ft_isdigit(input[i]) != 1)
+			return (0); // return error message?
+		i++;
+	}
+	if (ft_atoi(input) > 2147483647 || ft_atoi(input) < -2147483648)
+		return (0); // return error message?
+	return (1);
+}
+
+int	ft_isrepeat(char *input)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = i + 1;
+	while (input[i] != '\0')
+	{
+		while (input[i] != input[j])
+		{
+			if (input[i] == input[j])
+				return (0) // return error message?
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+void	input_conversion(int input)
+{
+	int	i;
+	t_node_list	*lst_a;
+
+	i = 0;
+	while (input)
+	{
+		ft_lstadd_front(lst_a, input[i]);
 		i++;
 	}
 }
