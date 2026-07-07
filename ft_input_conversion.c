6be88ce1246e744593c2419642886d8aef196843
thiasix <prg202@gmail.com>
2026-07-07 15:32:30 +0200

    radix start

--- ft_input_conversion.c
+++ ft_input_conversion.c
@@ -6,50 +6,50 @@
 /*   By: patperez <patperez@student.42urduliz.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/19 09:58:07 by patperez          #+#    #+#             */
-/*   Updated: 2026/07/07 08:49:23 by patperez         ###   ########.fr       */
+/*   Updated: 2026/07/07 09:12:29 by patperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pushswaplib.h"
 
-int	ft_is_validint(char *argv)
+int	ft_is_validint(char **argv)
 {
 	int	i;
 
 	i = 0;
-	printf("INPUT %s\n", argv);
-	if (input[i] == '-')
+	printf("INPUT %s\n", *argv);
+	if (*argv[i] == '-')
 		i++;
-	while (argv[i])
+	while (*argv[i])
 	{
-		if (!ft_isdigit(argv[i]))
+		if (!ft_isdigit(*argv[i]))
 		{
 			printf("ERROR: NOT A DIGIT\n");
 			return (0); // return error message?
 		}
 		i++;
 	}
-	if (ft_atoi(argv) < -2147483648 && ft_atoi(argv) > 2147483647)
+	if (ft_atoi(*argv) > -2147483648 && ft_atoi(*argv) > 2147483647)
 	{
-		printf("ERROR: OVERFLOW\N");
+		printf("ERROR: OVERFLOW\n");
 		return (0); // return error message?
 	}
 	return (1);
 }
 
-int	ft_isrepeat(char *input)
+/*int	ft_isrepeat(char **argv)
 {
 	int	i;
 	int	j;
 
 	i = 0;
 	j = i + 1;
-	while (input[i] != '\0')
+	while (argv[i] != '\0')
 	{
-		while (input[i] != input[j])
+		while (argv[i] != argv[j])
 		{
-			if (input[i] == input[j])
-				return (0) // return error message?
+			if (argv[i] == argv[j])
+				return (0); // return error message?
 			j++;
 		}
 		i++;
@@ -68,4 +68,4 @@ void	input_conversion(int input)
 		ft_lstadd_front(lst_a, input[i]);
 		i++;
 	}
-}
+}*/
---
+++ ft_radix.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_radix.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: patperez <patperez@student.42urduliz.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/07 09:19:02 by patperez          #+#    #+#             */
+/*   Updated: 2026/07/07 15:32:15 by patperez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pushswaplib.h"
+
+int	get_max_bits(t_stack *lst)
+{
+	t_node_list	*copy;
+	int	i;
+	int	bits;
+
+	i = 0;
+	bites = 0;
+	copy = *lst -> head;
+	while (copy -> next != head)
+	{
+		while (*copy[i])
+		{
+			bits++;
+			i++;
+		}
+		if (bits > i)
+			bits = i;
+		i = 0;
+		copy = copy -> next;
+	}
+	return (bits);
+}
+
+void	ft_radix(t_stack **lst_a, t_stack **lst_b)
+{
+	t_node_list	*head_mark;
+	int	i;
+	int	j;
+	int	size;
+	int	max_bits;
+
+	i = 0;
+	head_mark = *lst_a;
+	size = ft_lstsize(lst_a);
+	max_bits = get_max_bits(lst_a);
+	while (i < max_bits)
+	{
+		j = 0;
+		while (j++ < size)
+		{
+			head_mark = *lst_a;
+			if (((head_a -> index >> i) & 1) == 1)
+				ra(lst_a);
+			else
+				pb(lst_b, lst_a);
+		}
+		while (*lst_b)
+			pa(lst_a, lst_b);
+		ft_lstclear(lst_b);
+		i++;
+	}
+}
