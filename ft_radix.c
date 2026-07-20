fc5f9836257dd3030661738aad6fdc5b26810a2d
Unai Garcia Morchon <ugarcia-@c3r2s2.42urduliz.com>
2026-07-20 11:00:59 +0200

    creemos que funciona bien

--- ft_radix.c
+++ ft_radix.c
@@ -6,7 +6,7 @@
 /*   By: ugarcia- <ugarcia-@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/07 09:19:02 by patperez          #+#    #+#             */
-/*   Updated: 2026/07/20 07:49:54 by ugarcia-         ###   ########.fr       */
+/*   Updated: 2026/07/20 10:59:20 by ugarcia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,18 @@ void	ft_radix(t_stack *lst_a, t_bench *bench)
 	int			i;
 	int			j;
 	int			max_bits;
+	int			size;
 
 	lst_b = ft_newstack();
+	ft_get_index(lst_a);
 	i = 0;
+	size = lst_a -> size;
 	max_bits = get_max_bits(lst_a->size - 1);
 	while (i < max_bits)
 	{
 		j = 0;
-		while (j < lst_a -> size)
+
+		while (j < size)
 		{
 			if (((lst_a -> head -> index >> i) & 1) == 1)
 				ft_ra(lst_a, bench);
